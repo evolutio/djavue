6c12862b65c1ade2797a5dd7c434e2960c4f4989
tonylampada <tonylampada@gmail.com>
2018-03-13 18:05:12 -0300

    test metalsmith

---
+++ meta.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "label": "Project name"
+    },
+    "description": {
+      "type": "string",
+      "required": true,
+      "label": "Project description",
+      "default": "A Vue.js project"
+    }
+  },
+  "skipInterpolation": "frontend/**/*.vue",
+  "completeMessage": "{{#inPlace}}To get started:\n\n  npm install\n  npm run dev.{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev.{{/inPlace}}",
+  "metalsmith": function (metalsmith, opts, helpers) {
+    function customMetalsmithPlugin (files, metalsmith, done) {
+      console.log('files')
+      console.log(files)
+      done(null, files)
+    }
+    metalsmith.use(customMetalsmithPlugin)
+  }
+}
