f4ad6a1af8f1701420f0a4726b2f657fac7fe502
tonylampada <tonylampada@gmail.com>
2018-03-13 23:29:03 -0300

    teste

--- meta.js
+++ meta.js
@@ -20,14 +20,14 @@ module.exports = {
   },
   "skipInterpolation": "frontend/**/*.vue",
   // "completeMessage": "{{#inPlace}}To get started:\n\n  npm install\n  npm run dev.{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev.{{/inPlace}}",
-  "metalsmith": function (metalsmith, opts, helpers) {
-    function customMetalsmithPlugin (files, metalsmith, done) {
-      console.log('files')
-      console.log(files)
-      done(null, files)
-    }
-    metalsmith.use(customMetalsmithPlugin)
-  },
+  // "metalsmith": function (metalsmith, opts, helpers) {
+  //   function customMetalsmithPlugin (files, metalsmith, done) {
+  //     console.log('files')
+  //     console.log(files)
+  //     done(null, files)
+  //   }
+  //   metalsmith.use(customMetalsmithPlugin)
+  // },
   complete: function (data, {logger, chalk}) {
     const msg = `${chalk.green('completeEEEEEEEE')}`
     console.log(msg)
