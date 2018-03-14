b860c0bd79943cb79aa69778851f92e42534239d
tonylampada <tonylampada@gmail.com>
2018-03-14 19:35:35 -0300

    complete msg

--- meta.js
+++ meta.js
@@ -21,9 +21,20 @@ module.exports = {
   "skipInterpolation": "frontend/**/*.vue",
   // "completeMessage": "{{#inPlace}}To get started:\n\n  npm install\n  npm run dev.{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev.{{/inPlace}}",
   "complete": function (data, {logger, chalk}) {
-    const msg = `${chalk.green('COMPLETE!')}`
-    logger.log(msg)
     const base = data.inPlace ? '' : data.destDirName + '/'
     fs.rename(`${base}__name__`, `${base}${data.name}`, onerror)
+    cmds = data.inPlace ? '' : `cd ${data.destDirName}\n  `
+    cmds += `source dev.sh
+  setup_help`
+    const message = `
+# ${chalk.green('Project initialization finished!')}
+# ========================
+To get started:
+  
+  ${chalk.yellow(cmds)}
+
+Documentation can be found at https://vuejs-templates.github.io/webpack
+`
+    console.log(message)
   },
 }
