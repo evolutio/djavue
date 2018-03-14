f1e45e7c2e71625d2464765e006a3181814fe03b
tonylampada <tonylampada@gmail.com>
2018-03-14 10:58:09 -0300

    aprendi a renomear

--- meta.js
+++ meta.js
@@ -5,16 +5,6 @@ function onerror(err) {
 }
 
 module.exports = {
-  "complete": function (data, {logger, chalk}) {
-    const msg = `${chalk.green('completeEEEEEEEE')}`
-    logger.log(msg)
-    fs.rename('__name__', '__bilu__')
-    // logger.log('-----------complete')
-    // logger.log(data)
-    // if (!data.inPlace) {
-    //   logger.log(`cd ${chalk.yellow(data.destDirName)}`)
-    // }
-  },
   "prompts": {
     "name": {
       "type": "string",
@@ -27,32 +17,13 @@ module.exports = {
       "label": "Project description",
       "default": "A Vue.js project"
     }
-    // "goiaba": {
-    //   "type": "string",
-    //   "required": true,
-    //   "label": "Quer goiaba?",
-    //   "default": "Sim"
-    // }
   },
   "skipInterpolation": "frontend/**/*.vue",
   // "completeMessage": "{{#inPlace}}To get started:\n\n  npm install\n  npm run dev.{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev.{{/inPlace}}",
-  // "metalsmith": function (metalsmith, opts, helpers) {
-  //   function customMetalsmithPlugin (files, metalsmith, done) {
-  //     console.log('files')
-  //     console.log(files)
-  //     done(null, files)
-  //   }
-  //   metalsmith.use(customMetalsmithPlugin)
-  // },
   "complete": function (data, {logger, chalk}) {
-    const msg = `${chalk.green('completeEEEEEEEE')}`
+    const msg = `${chalk.green('COMPLETE!')}`
     logger.log(msg)
     const base = data.inPlace ? '' : data.destDirName + '/'
     fs.rename(`${base}__name__`, `${base}${data.name}`, onerror)
-    // logger.log('-----------complete')
-    // logger.log(data)
-    // if (!data.inPlace) {
-    //   logger.log(`cd ${chalk.yellow(data.destDirName)}`)
-    // }
   },
 }
