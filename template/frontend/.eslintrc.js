124bb02ddf87ee82b78e4629468d71a73fc0ed87
Roger Camargo <huogerac@gmail.com>
2022-09-20 17:13:41 -0300

    Fix setup frontend

--- template/frontend/.eslintrc.js
+++ template/frontend/.eslintrc.js
@@ -4,20 +4,15 @@ module.exports = {
     browser: true,
     node: true
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/recommended',
   ],
   // required to lint *.vue files
   plugins: [
-    'vue'
   ],
   // add your custom rules here
   rules: {
-    "vue/valid-v-for": "off"
   }
 }
