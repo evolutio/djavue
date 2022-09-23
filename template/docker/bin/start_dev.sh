e7ff74dacb7ac804751bf39fa40825a832dd62ac
Roger Camargo <huogerac@gmail.com>
2022-09-23 10:46:19 -0300

    Revert "Adiciona devcontainer para conseguir fazer debug"
    This reverts commit c478f9a010a72d34e7b454afb273c70e4abc8557.

--- template/docker/bin/start_dev.sh
+++ template/docker/bin/start_dev.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-wait-for-it $POSTGRES_HOST:$POSTGRES_PORT
-
 ./manage.py collectstatic --noinput
 ./manage.py migrate --noinput
+
+cd /app/frontend
+#NODE_ENV=development PORT=3001 HOST=0.0.0.0 API_MOCK=1 npm run dev &
+npm run dev &
 
 cd /app
 DEBUG=1 ./manage.py runserver 0.0.0.0:8000
