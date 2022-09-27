daeea59cb1e52e5e67b1d3f017e4d43559824b59
Roger Camargo <huogerac@gmail.com>
2022-09-26 21:59:35 -0300

    Adiciona conf. de linter no frontend

--- template/frontend/.eslintrc.js
+++ template/frontend/.eslintrc.js
@@ -7,12 +7,26 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/recommended',
+    'standard',
+    'plugin:vue/recommended'
   ],
   // required to lint *.vue files
   plugins: [
   ],
   // add your custom rules here
   rules: {
+    'no-debugger': 0,
+    camelcase: 0,
+    'require-await': 'error',
+    'vue/no-unused-properties': ['error', {
+      groups: ['props', 'data', 'computed', 'methods'],
+      ignorePublicMembers: true
+    }],
+    'vue/no-unused-refs': 'error',
+    'vue/this-in-template': 'error',
+    'vue/order-in-components': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/no-v-html': 'off'
   }
 }
